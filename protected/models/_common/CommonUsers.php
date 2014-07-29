08df6361e14a5c341408839a6ec40a3a68fe1ac5
tom <tom@simb.com.au>
2014-07-29 14:36:24 +0700

    enhance user update

--- protected/models/_common/CommonUsers.php
+++ protected/models/_common/CommonUsers.php
@@ -14,6 +14,7 @@ class CommonUsers extends BaseUsers
     	return array(
         		//Applies to 'update' scenario
     			array('username', 'required'),
+    			array('type', 'required'),
     			array('password', 'required' ,'except' => 'update'),
     	);
     }
