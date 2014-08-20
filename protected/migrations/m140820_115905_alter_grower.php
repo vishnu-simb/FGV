3ef769e1fd2349ae914437dd385de895670a4887
tom <tom@simb.com.au>
2014-08-20 20:32:03 +0700

    add migration alter grower table (please run migrate to change scripts)

--- protected/migrations/m140820_115905_alter_grower.php
+++ protected/migrations/m140820_115905_alter_grower.php
@@ -2,17 +2,45 @@
 
 class m140820_115905_alter_grower extends CDbMigration
 {
+	
+	public $tableNameGrower = 'grower';
+	
+	public function updateTablePrefix()
+	{
+		$this->tableNameGrower = $this->dbConnection->tablePrefix . $this->tableNameGrower;
+	}
+	
 	public function up()
 	{
-		// Common fields, should appear in all table
+		$this->updateTablePrefix();
+		
+		$this->addColumn($this->tableNameGrower,'contact_name', 'VARCHAR(100) NULL AFTER salt');
+		$this->addColumn($this->tableNameGrower,'address', 'VARCHAR(255) NULL AFTER contact_name');
+		$this->addColumn($this->tableNameGrower,'suburb', 'VARCHAR(50) AFTER address');
+		$this->addColumn($this->tableNameGrower,'postcode', 'VARCHAR(5) AFTER suburb');
+		$this->addColumn($this->tableNameGrower,'state', "ENUM('ACT','NSW','NT','QLD','SA','TAS','VIC','WA') AFTER postcode");
+		$this->addColumn($this->tableNameGrower,'phone', 'VARCHAR(20) AFTER state');
+		$this->addColumn($this->tableNameGrower,'mobile', 'VARCHAR(20) AFTER state');
 		$this->addColumn($this->tableNameGrower,'avatar', 'varchar(255) NULL AFTER mobile');
+		
 		return true;
 	}
 
 	public function down()
 	{
-		echo "m140820_115905_alter_grower does not support migration down.\n";
-		return false;
+		$this->updateTablePrefix();
+		
+		$this->dropColumn($this->tableNameGrower,'contact_name');
+		$this->dropColumn($this->tableNameGrower,'address');
+		$this->dropColumn($this->tableNameGrower,'suburb');
+		$this->dropColumn($this->tableNameGrower,'postcode');
+		$this->dropColumn($this->tableNameGrower,'state');
+		$this->dropColumn($this->tableNameGrower,'phone');
+		$this->dropColumn($this->tableNameGrower,'mobile');
+		$this->dropColumn($this->tableNameGrower,'avatar');
+		
+		echo "m140820_115905_alter_grower migration down.\n";
+		return true;
 	}
 
 	/*
