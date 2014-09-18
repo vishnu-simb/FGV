30a67c3680535cd483a2266375bacebdb34ee641
tom <tom@simb.com.au>
2014-09-18 16:42:07 +0700

    Fixed PEAR Exception

--- protected/extensions/BOM/Services_JSON.php
+++ protected/extensions/BOM/Services_JSON.php
@@ -905,23 +905,8 @@ class Services_JSON
 
 }
 
-if (class_exists('PEAR_Error')) {
 
-    class Services_JSON_Error extends PEAR_Error
-    {
-        function Services_JSON_Error($message = 'unknown error', $code = null,
-                                     $mode = null, $options = null, $userinfo = null)
-        {
-            parent::PEAR_Error($message, $code, $mode, $options, $userinfo);
-        }
-    }
-
-} else {
-
-    /**
-     * @todo Ultimately, this class shall be descended from PEAR_Error
-     */
-    class Services_JSON_Error
+   class Services_JSON_Error
     {
         function Services_JSON_Error($message = 'unknown error', $code = null,
                                      $mode = null, $options = null, $userinfo = null)
@@ -930,4 +915,4 @@ if (class_exists('PEAR_Error')) {
         }
     }
     
-}
+
