b82b3f8762283c2681a76d3376187f87f35c1659
Andrew Chapman <andy@simb.com.au>
2014-09-26 14:08:43 +1000

    Adding cron task

--- protected/commands/TestCommand.php
+++ protected/commands/TestCommand.php
@@ -21,4 +21,34 @@ class TestCommand extends SimbConsoleCommand
         echo "index result: \n";
         echo "sdaf: \n";
     }
+    
+    public function run($args)
+    {    
+        $this->msg("Running Test command");
+        
+        $pest_id = 3;
+        $block_id = 220;
+        $date = "2014-08-29";
+        
+        $format = Yii::app()->params['dbDateFormat'];
+        $postData = Yii::app()->request->getPost('Biofix');
+        $pestSpray = PestSpray::model()->findAllByAttributes(array('pest_id'=>$pest_id));
+        $block = Block::model()->findByAttributes(array('id'=>$block_id));
+        $spraydates = array();        
+        
+        $this->msg("Loaded pests and blocks");
+        
+        foreach($pestSpray as $key=>$vv){
+            $this->msg("Processing Pest Spray: " . $vv->id);    
+            $spraydates[$vv->id]= $vv->getDate($block, false, date('Y',strtotime($date)),true);
+        }
+        
+        print_r($spraydates);
+        
+    }
+    
+    public function msg($m)
+    {
+        echo $m . "\n";
+    }
 }
---
+++ protected/commands/UpdateBiofixCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Author: Andrew Chapman
+ * Date: 26/09/2014
+ * Time: 12:55 PM
+ *
+ */
+class UpdateBiofixCommand extends SimbConsoleCommand
+{
+    public function run($args)
+    {    
+        $this->msg("Getting all biofix entries that have dates greater than or equal to 180 days ago");
+        
+        $threshold = date("Y-m-d", time() - (86400 * 180));
+        
+        $rstBioFix = Biofix::model()->findAll(array(
+            'condition'=>'date >= :date',
+            'params'=>array(':date'=>$threshold),
+        )); 
+        
+        foreach($rstBioFix as $objBiofix) {
+            $pest_id = $objBiofix->pest_id;
+            $block_id = $objBiofix->block_id;
+            $date = $objBiofix->date;      
+            
+            $this->msg("Processing Biofix id: " . $objBiofix->id . ", Pest ID: " . $pest_id. ", Block: " . $block_id . ", Date: " . $date);      
+
+            $format = Yii::app()->params['dbDateFormat'];
+            $pestSpray = PestSpray::model()->findAllByAttributes(array('pest_id'=>$pest_id));
+            $block = Block::model()->findByAttributes(array('id'=>$block_id));
+            $spraydates = array();        
+            
+            $this->msg("Loaded pests and blocks");
+            
+            foreach($pestSpray as $key=>$vv){
+                $this->msg("Processing Pest Spray: " . $vv->id);    
+                $spraydates[$vv->id]= $vv->getDate($block, false, date('Y',strtotime($date)),true);
+                
+                print_r($spraydates);
+                
+                $objBiofix->params = CJSON::encode($spraydates);
+                $objBiofix->update();                
+            }            
+            
+        }       
+    }
+    
+    public function msg($m)
+    {
+        echo $m . "\n";
+    }
+}
