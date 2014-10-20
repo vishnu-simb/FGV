a89a6b63085128007cc4271ed783a431cfdfdbed
tom <tom@simb.com.au>
2014-10-20 22:18:46 +0700

    Added scroll right arrow

--- protected/views/backend/layouts/_scroll_to_right.php
+++ protected/views/backend/layouts/_scroll_to_right.php
@@ -7,17 +7,37 @@
  * render main message for the layout
  */
 Yii::app()->clientScript->registerScript(
-    'hideMessage',
-    <<<JS_SCRIPT
-        setTimeout(function(){
-            $('#scroll_to_right').animate({opacity: 1.0}, 3000).fadeOut("slow");
-        }, 3000);
-JS_SCRIPT
-    ,
-    CClientScript::POS_READY
+    'scroll',"
+		$.fn.hasOverflow=function(){var e=this[0];return e.scrollHeight>e.clientHeight||e.scrollWidth>e.clientWidth;}
+    		
+    	jQuery(document).ready(function() {
+
+    		if(jQuery('.dataTables_wrapper').hasOverflow()){
+    			jQuery('#scroll_to_right').removeClass('hidden');
+			}else{
+    			jQuery('#scroll_to_right').addClass('hidden');
+			}
+			jQuery('.left-arrow').click(function () {
+			  var leftPos = $('.dataTables_wrapper').scrollLeft();
+			  jQuery('.dataTables_wrapper').animate({scrollLeft: leftPos - 1200}, 800);
+    		  jQuery(this).addClass('hidden');
+    		  jQuery('.right-arrow').removeClass('hidden');
+			});
+			 
+			jQuery('.right-arrow').click(function () {
+			  var leftPos = $('.dataTables_wrapper').scrollLeft();
+			  jQuery('.dataTables_wrapper').animate({scrollLeft: leftPos + 1200}, 800);
+    		  jQuery(this).addClass('hidden');
+    		  jQuery('.left-arrow').removeClass('hidden');
+			});
+            });
+    		
+    		"
 );
 ?>
 <div id="scroll_to_right">
- 	<button class="btn btn-blue hidden"><i class="glyphicon-chevron-left"></i></button>
-    <button class="btn btn-blue"><i class="glyphicon-chevron-right"></i></button>
+     <button class="btn btn-blue left-arrow hidden"><i class="glyphicon-chevron-left"></i></button>
+     <button class="btn btn-blue right-arrow"><i class="glyphicon-chevron-right"></i></button>
 </div>
+
+
